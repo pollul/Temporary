a92215595337f9d7928aed1155c544a82d32f5be
KT Salon User <user@example.com>
2025-08-08 18:53:36 -0700

    build: initialize Next.js project with webpack configuration and static files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27eb0bc7aef8372a"; }
+/******/ 		__webpack_require__.h = function() { return "c1b438d1167c0462"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
